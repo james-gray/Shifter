3adedafafb00a781e2d0dafa470e0ddab8135b66
jorshi <jordieshier@gmail.com>
2016-07-28 20:15:39 -0700

    Beginning of GUI

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -10,7 +10,7 @@
 
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
-
+#include <string>
 
 //==============================================================================
 ShifterAudioProcessorEditor::ShifterAudioProcessorEditor (ShifterAudioProcessor& p)
@@ -18,7 +18,36 @@ ShifterAudioProcessorEditor::ShifterAudioProcessorEditor (ShifterAudioProcessor&
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    setSize (300, 165);
+    
+    // Set the coarse pitch rotary
+    coarsePitch.setSliderStyle(Slider::RotaryVerticalDrag);
+    coarsePitch.setRange(-12.0, 12.0, 1.0);
+    coarsePitch.setTextBoxStyle(Slider::NoTextBox, true, 90, 0);
+    coarsePitch.setPopupDisplayEnabled(true, this);
+    coarsePitch.setTextValueSuffix(" Semitones");
+    coarsePitch.setColour(0x1001311, Colours::black);
+    coarsePitch.setValue(0.0);
+    
+    // Set the fine pitch rotary
+    finePitch.setSliderStyle(Slider::RotaryVerticalDrag);
+    finePitch.setRange(-50.0, 50.0, 1.0);
+    finePitch.setTextBoxStyle(Slider::NoTextBox, true, 90, 0);
+    finePitch.setPopupDisplayEnabled(true, this);
+    finePitch.setTextValueSuffix(" Cents");
+    finePitch.setColour(0x1001311, Colours::black);
+    finePitch.setValue(0.0);
+    
+    const File logoFile(String("./Images/shifter_logo2.png"));
+    Image ourLogo = ImageFileFormat::loadFrom(logoFile);
+    logo.setImage(ourLogo);
+    
+    
+    
+    // Add the two rotaries to the GUI
+    addAndMakeVisible(&coarsePitch);
+    addAndMakeVisible(&finePitch);
+    addAndMakeVisible(&logo);
 }
 
 ShifterAudioProcessorEditor::~ShifterAudioProcessorEditor()
@@ -28,15 +57,17 @@ ShifterAudioProcessorEditor::~ShifterAudioProcessorEditor()
 //==============================================================================
 void ShifterAudioProcessorEditor::paint (Graphics& g)
 {
-    g.fillAll (Colours::white);
+    g.fillAll (Colours::darkgrey);
 
     g.setColour (Colours::black);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), Justification::centred, 1);
 }
 
 void ShifterAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    coarsePitch.setBounds(65, 40, 75, getHeight() - 25);
+    finePitch.setBounds(170, 40, 50, getHeight() - 25);
+    logo.setBounds(0, 0, 300, 60);
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -32,6 +32,10 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     ShifterAudioProcessor& processor;
+    
+    Slider coarsePitch;
+    Slider finePitch;
+    ImageComponent logo;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ShifterAudioProcessorEditor)
 };
